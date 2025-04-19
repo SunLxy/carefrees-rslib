6ded57bab8f65af54b5436846b0320954c9f412a
SunLxy <1011771396@qq.com>
2025-04-19 22:32:52 +0800

    fix:修改命令参数

--- src/init.ts
+++ src/init.ts
@@ -6,7 +6,7 @@ const __dirname = path.dirname(__filename);
 
 export const initProcessConfig = (): void => {
   const isConfig = process.argv.find((item) => item.includes('--config') || item.includes('-c'))
-  const isNode = process.argv.find((item) => item.includes('--target'))
+  const isNode = process.argv.find((item) => item.includes('--node'))
   if (!isConfig) {
     process.argv.push('--config', path.resolve(__dirname, "rslib.config.js"))
   }
