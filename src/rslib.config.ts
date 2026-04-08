386e4c707bf2dce4e5d8390a54bdd27ec9f5541c
SunLxy <1011771396@qq.com>
2026-04-08 18:39:05 +0800

    released v1.0.0

--- src/rslib.config.ts
+++ src/rslib.config.ts
@@ -1,29 +1,66 @@
-import { pluginReact } from "@rsbuild/plugin-react";
-import { pluginSvgr } from "@rsbuild/plugin-svgr";
+// import { pluginReact } from "@rsbuild/plugin-react";
+// import { pluginSvgr } from "@rsbuild/plugin-svgr";
+// import { pluginUnpluginVue } from "rsbuild-plugin-unplugin-vue";
+// import { pluginVueTsc } from "./plugins/vue-tsc";
+// import { pluginSass } from "@rsbuild/plugin-sass";
 import { defineConfig, RslibConfig } from "@rslib/core";
-import { pluginUnpluginVue } from "rsbuild-plugin-unplugin-vue";
-import { pluginVueTsc } from "./plugins/vue-tsc";
-import { pluginSass } from "@rsbuild/plugin-sass";
 
-export default defineConfig(() => {
+export default defineConfig(async function () {
   const isNode = process.env.CAREFREE_RSLIB_TARGET === "node";
   const isOnlyESM = process.env.CAREFREE_RSLIB_ESM === "true";
   const isVue = process.env.CAREFREE_RSLIB_VUE === "true";
   const isSass = process.env.CAREFREE_RSLIB_SASS === "true";
-  let plugins = isNode
-    ? []
-    : [
-        pluginReact(),
-        pluginSvgr({
-          mixedImport: true,
-          svgrOptions: { exportType: "named" },
-        }),
-      ];
-  if (isVue) {
-    plugins = [pluginUnpluginVue({}), pluginVueTsc()];
+  let plugins = []
+  if (!isNode && !isVue) {
+    try {
+      const reactPlugin = await import("@rsbuild/plugin-react");
+      plugins.push(reactPlugin.pluginReact());
+    } catch (error) {
+      console.log(error)
+    }
+    try {
+      const svgrPlugin = await import("@rsbuild/plugin-svgr");
+      plugins.push(svgrPlugin.pluginSvgr({
+        mixedImport: true,
+        svgrOptions: { exportType: "named" },
+      }));
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  // let plugins = isNode
+  //   ? []
+  //   : [
+  //     pluginReact(),
+  //     pluginSvgr({
+  //       mixedImport: true,
+  //       svgrOptions: { exportType: "named" },
+  //     }),
+  //   ];
+  if (!isNode && isVue) {
+    plugins = []
+    try {
+      const vuePlugin = await import("rsbuild-plugin-unplugin-vue");
+      plugins.push(vuePlugin.pluginUnpluginVue({}));
+    } catch (error) {
+      console.log(error)
+    }
+    try {
+      const vuePlugin2 = await import("./plugins/vue-tsc");
+      plugins.push(vuePlugin2.pluginVueTsc());
+    } catch (error) {
+      console.log(error)
+    }
+    // plugins = [pluginUnpluginVue({}), pluginVueTsc()];
   }
   if (isSass) {
-    plugins.push(pluginSass());
+    try {
+      const sassPlugin = await import("@rsbuild/plugin-sass");
+      plugins.push(sassPlugin.pluginSass());
+    } catch (error) {
+      console.log(error)
+    }
+    // plugins.push(pluginSass());
   }
   return {
     source: {
@@ -48,18 +85,18 @@ export default defineConfig(() => {
       isOnlyESM
         ? undefined
         : {
-            bundle: false,
-            dts: isVue ? false : true,
-            format: "cjs",
-            output: {
-              filename: {
-                js: "[name].js",
-              },
-              distPath: {
-                root: "./lib",
-              },
+          bundle: false,
+          dts: isVue ? false : true,
+          format: "cjs",
+          output: {
+            filename: {
+              js: "[name].js",
+            },
+            distPath: {
+              root: "./lib",
             },
           },
+        },
     ].filter(Boolean),
     output: {
       target: isNode ? "node" : "web",
